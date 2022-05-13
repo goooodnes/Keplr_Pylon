d94bf13d74554668131696831cdf350bb0f0ecee
Alexey <77731459+goooodnes@users.noreply.github.com>
2022-05-13 21:26:13 +0300

    Update pylon.js

--- pylon.js
+++ pylon.js
@@ -1,4 +1,8 @@
-
+window.onload = async () => {
+    if (!window.keplr) {
+        alert("Please install keplr extension");
+    } else 
+        
     await window.keplr.experimentalSuggestChain({
     chainId: "pylons-testnet-3",
     chainName: "PYLON",
